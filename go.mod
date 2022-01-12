12d8c0db7e5216a273d41b3be61e702c6f45c4ea
tarasov <tarasov@o-code.ru>
2022-01-12 08:23:50 +0400

    first implement reader and parser

---
+++ go.mod
@@ -0,0 +1,5 @@
+module glink
+
+go 1.17
+
+require golang.org/x/net v0.0.0-20220111093109-d55c255bac03
--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+    "os"
     "fmt"
+    "log"
+    "bufio"
+//    "strings"
+    "golang.org/x/net/html"
 )
 
 
+
 type Link struct {
-    Href string
-    Text string
+    Href, Text string
 }
 
 func main() {
@@ -17,4 +22,35 @@ func main() {
     }
 
     fmt.Printf("%s\n%s\n", l.Href, l.Text)
+
+
+    file, err := os.Open("ex1.html")
+    if err != nil {
+         log.Fatal(err)
+    }
+    defer file.Close()
+    rd := bufio.NewReader(file)
+
+    doc, err := html.Parse(rd)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+//    fmt.Println(doc)
+
+    var f func(*html.Node)
+    f = func(n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+	    for _, a := range n.Attr {
+		if a.Key == "href" {
+		    fmt.Println(a.Val)
+		    break
+		}
+	    }
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+	    f(c)
+	}
+    }
+    f(doc)
 }
