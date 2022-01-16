4e6d79b73b83b71ddfeab7c862bb2fa0d116b7b4
raptor72 <mctaras51@yandex.ru>
2022-01-16 22:23:24 +0400

    Added a separate function to parse a HTML

--- main.go
+++ main.go
@@ -21,6 +21,27 @@ func readFile(filename string) (*os.File, error) {
     return file, err
 }
 
+func parseHTML(n *html.Node, l *[]Link) {
+    // var links []Link
+    // var l []Link
+    if n.Type == html.ElementNode && n.Data == "a" {
+        for _, a := range n.Attr {
+            if a.Key == "href" {
+                if n.FirstChild.Type == html.TextNode {
+                    // l = Link{a.Val, n.FirstChild.Data}                   
+                    *l = append(*l, Link{a.Val, n.FirstChild.Data})
+                    // fmt.Println(l)
+                }
+            break
+            }
+        }   
+    }
+    for c := n.FirstChild; c != nil; c = c.NextSibling {
+        parseHTML(c, l)
+    }
+}
+
+
 func researchNode(n *html.Node) {
     fmt.Printf("node.Type: %v - %T\n", n.Type, n.Type)
     fmt.Printf("node.DataAtom: %v - %T\n", n.DataAtom, n.DataAtom)
@@ -34,11 +55,11 @@ func main() {
     filename := flag.String("filename", "ex1.html", "a string filename to parse a html")
     flag.Parse()
 
-    l := Link{
-        Href: "/dog",
-        Text: "Something in a span Text not in a span Bold text!",
-    }
-    fmt.Sprintf("%s\n%s\n", l.Href, l.Text)
+    // l := Link{
+    //     Href: "/dog",
+    //     Text: "Something in a span Text not in a span Bold text!",
+    // }
+    // fmt.Sprintf("%s\n%s\n", l.Href, l.Text)
 
     file, err := readFile(*filename)
     if err != nil {
@@ -54,32 +75,32 @@ func main() {
 
 //    fmt.Printf("%T\n", doc)
 
-    var links []Link
-    var f func(*html.Node)
+    var lks []Link
+    // var f parseHTML(*html.Node)
+    // f = func(n *html.Node) {
+    //     // fmt.Println(n.Data, n.Type)
+    //     if n.Type == html.ElementNode && n.Data == "a" {
+    // 	    for _, a := range n.Attr {
+    //         // fmt.Println(a)
+    //             if a.Key == "href" {
+	// 	            // fmt.Println(a.Val)
+    //                 if n.FirstChild.Type == html.TextNode {
+    //                     // fmt.Println(n.FirstChild.Data)
+    //                     l = Link{a.Val, n.FirstChild.Data}
+    //                     // fmt.Println(l)
+    //                     links = append(links, l)
+    //                 }
+    //                 break
+	// 	        }
+    //         }
+	//     }
+	//     for c := n.FirstChild; c != nil; c = c.NextSibling {
+    // 	    f(c)
+	//     }
+    // }
+    // f(doc)
 
-    f = func(n *html.Node) {
-        // fmt.Println(n.Data, n.Type)
-            if n.Type == html.ElementNode && n.Data == "a" {
-    	    for _, a := range n.Attr {
-            // fmt.Println(a)
-                if a.Key == "href" {
-		            // fmt.Println(a.Val)
-                    if n.FirstChild.Type == html.TextNode {
-                        // fmt.Println(n.FirstChild.Data)
-                        l = Link{a.Val, n.FirstChild.Data}
-                        // fmt.Println(l)
-                        links = append(links, l)
-                    }
-                    break
-		        }
-            }
-	    }
-	    for c := n.FirstChild; c != nil; c = c.NextSibling {
-    	    f(c)
-	    }
-    }
-    f(doc)
-
-    fmt.Println(links)
+    parseHTML(doc, &lks)
+    fmt.Println(lks)
     researchNode(doc)
 }
