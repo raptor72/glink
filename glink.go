fbe798062997c5062a3c3b9fda4ee585beddf19c
tarasov <tarasov@o-code.ru>
2022-01-20 09:51:26 +0400

    correct triming spaces

--- glink.go
+++ glink.go
@@ -29,7 +29,7 @@ func dfsText(n *html.Node) string {
         s += n.Data
     }
     for c := n.FirstChild; c != nil; c = c.NextSibling {
-        s += dfsText(c)
+        s += dfsText(c) + " "
     }
     return strings.Join(strings.Fields(s), " ")
 }
@@ -77,4 +77,5 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("%+v\n", links)
+	// fmt.Println(links)
 }
