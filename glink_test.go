d5f6f2b06cb5f1a202d10a71af48de761610a84b
raptor72 <mctaras51@yandex.ru>
2022-01-22 15:41:33 +0400

    added tests for simple structs

--- glink_test.go
+++ glink_test.go
@@ -1,19 +1,18 @@
 package glink
 
 import (
-    "fmt"
 	"strings"
 	"testing"
+	"fmt"
+
 	"golang.org/x/net/html"
 )
-
 
 func createNode(content string) (n *html.Node) {
 	n, err := html.Parse(strings.NewReader(content))
 	if err != nil {
 		panic(err)
 	}
-    fmt.Println(n)
 	return n
 }
 
@@ -40,12 +39,47 @@ func createNode(content string) (n *html.Node) {
 // }
 
 func Test_TraverseText_WhenGivenPlainText_ShouldReturnText(t *testing.T) {
-	expected := "I am but text"
+	expected := "I am just text"
 	node := createNode(expected)
-
 	result := dfsText(node)
 
 	if result != expected {
 		t.Errorf("Expected [%s] to equal [%s]", result, expected)
 	}
 }
+
+func Test_dfsText_from_html_Node(t *testing.T) {
+	node_string := `<a href="/other-page">A link to another page</a>`
+    node := createNode(node_string)   
+    expected := "A link to another page"
+    result := dfsText(node)
+	if result != expected {
+		t.Errorf("Expected [%s] to equal [%s]", result, expected)
+	}
+} 
+
+func Test_dfsLink_from_simple_node(t *testing.T) {
+	var l []Link
+	exp2 := `<body>
+                  <h1>Hello!</h1>
+                  <a href="/other-page">A link to another page</a>
+             </body>`
+	node := createNode(exp2)
+	result := dfsLink(node, &l)
+	expected := "A link to another page"
+    expected_printed_type := "[]glink.Link"
+	expected_length := 1
+
+	got_type := fmt.Sprintf("%T", result)
+    got_length := len(result)
+	if got_type != expected_printed_type {
+		t.Errorf("Expected type [%s], but got [%s]", expected_printed_type, got_type)
+	}
+    if got_length != expected_length {
+		t.Errorf("Expected length [%d], but got [%d]", expected_length, got_length)
+	}
+	if result[0].Text != expected {
+		t.Errorf("Expected [%s], but got [%s]", expected, result)
+	}
+}
+
