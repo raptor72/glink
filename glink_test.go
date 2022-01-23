a30ddbb8b4b25157683f1e2d2bfff3e00f136d77
raptor72 <mctaras51@yandex.ru>
2022-01-23 11:47:54 +0400

    Added more tests

--- glink_test.go
+++ glink_test.go
@@ -16,28 +16,6 @@ func createNode(content string) (n *html.Node) {
 	return n
 }
 
-// func createAnchorNode(href string, text string) *html.Node {
-// 	textNode := createNode(text)
-// 	attrs := make([]html.Attribute, 1)
-// 	attrs[0] = html.Attribute{
-// 		Namespace: "",
-// 		Key:       "href",
-// 		Val:       href,
-// 	}
-// 	return &html.Node{
-// 		Parent:      nil,
-// 		FirstChild:  textNode,
-// 		LastChild:   textNode,
-// 		PrevSibling: nil,
-// 		NextSibling: nil,
-// 		Type:        html.ElementNode,
-// 		DataAtom:    0,
-// 		Data:        "a",
-// 		Namespace:   "",
-// 		Attr:        attrs,
-// 	}
-// }
-
 func Test_TraverseText_WhenGivenPlainText_ShouldReturnText(t *testing.T) {
 	expected := "I am just text"
 	node := createNode(expected)
@@ -57,16 +35,16 @@ func Test_dfsText_from_html_Node(t *testing.T) {
 		t.Errorf("Expected [%s] to equal [%s]", result, expected)
 	}
 } 
-
 func Test_dfsLink_from_simple_node(t *testing.T) {
 	var l []Link
-	exp2 := `<body>
+	node_string := `<body>
                   <h1>Hello!</h1>
                   <a href="/other-page">A link to another page</a>
              </body>`
-	node := createNode(exp2)
+	node := createNode(node_string)
 	result := dfsLink(node, &l)
-	expected := "A link to another page"
+	expected_text := "A link to another page"
+	expected_href := "/other-page"
     expected_printed_type := "[]glink.Link"
 	expected_length := 1
 
@@ -78,8 +56,170 @@ func Test_dfsLink_from_simple_node(t *testing.T) {
     if got_length != expected_length {
 		t.Errorf("Expected length [%d], but got [%d]", expected_length, got_length)
 	}
-	if result[0].Text != expected {
-		t.Errorf("Expected [%s], but got [%s]", expected, result)
+	if result[0].Text != expected_text {
+		t.Errorf("Expected text [%s], but got [%s]", expected_text, result[0].Text)
+	}
+    if result[0].Href != expected_href {
+		t.Errorf("Expected href [%s], but got [%s]", expected_href, result[0].Href)
 	}
 }
 
+func Test_dfsLink_from_node_with_text_in_tags(t *testing.T) {
+	var l []Link
+	node_string := `<html>
+	<body>
+	  <h1>Hello!</h1>
+	  <a href="/page-one">
+		  A link to first page
+		  <span> some span </span>
+	  </a>
+	</body>
+	</html>`
+	node := createNode(node_string)
+	result := dfsLink(node, &l)
+	expected_text := "A link to first page some span"
+	expected_href := "/page-one"
+	expected_printed_type := "[]glink.Link"
+	expected_length := 1
+
+	got_type := fmt.Sprintf("%T", result)
+    got_length := len(result)
+	if got_type != expected_printed_type {
+		t.Errorf("Expected type [%s], but got [%s]", expected_printed_type, got_type)
+	}
+    if got_length != expected_length {
+		t.Errorf("Expected length [%d], but got [%d]", expected_length, got_length)
+	}
+	if result[0].Text != expected_text {
+		t.Errorf("Expected [%s], but got [%s]", expected_text, result[0].Text)
+	}
+    if result[0].Href != expected_href {
+		t.Errorf("Expected href [%s], but got [%s]", expected_href, result[0].Href)
+	}
+}
+
+
+func Test_dfsLink_from_node_with_coment(t *testing.T) {
+	var l []Link
+	node_string := `<html>
+	<body>
+	  <a href="/dog-cat">dog cat <!-- commented text SHOULD NOT be included! --></a>
+	</body>
+	</html>`
+	node := createNode(node_string)
+	result := dfsLink(node, &l)
+	expected_text := "dog cat"
+	expected_href := "/dog-cat"
+	expected_printed_type := "[]glink.Link"
+	expected_length := 1
+
+	got_type := fmt.Sprintf("%T", result)
+    got_length := len(result)
+	if got_type != expected_printed_type {
+		t.Errorf("Expected type [%s], but got [%s]", expected_printed_type, got_type)
+	}
+    if got_length != expected_length {
+		t.Errorf("Expected length [%d], but got [%d]", expected_length, got_length)
+	}
+	if result[0].Text != expected_text {
+		t.Errorf("Expected [%s], but got [%s]", expected_text, result[0].Text)
+	}
+    if result[0].Href != expected_href {
+		t.Errorf("Expected href [%s], but got [%s]", expected_href, result[0].Href)
+	}
+}
+
+func Test_dfsLink_from_node_with_many_subnodes_same_depth(t *testing.T) {
+	var l []Link
+	node_string := `<html>
+	<head>
+	</head>
+	<body>
+		<a href="https://www.twitter.com/joncalhoun">
+		  Check me out on twitter
+		</a>
+		<a href="https://github.com/gophercises">
+		  Gophercises is on Github!
+		</a>
+	</body>
+	</html>`
+	node := createNode(node_string)
+	result := dfsLink(node, &l)
+	expected_1_text := "Check me out on twitter"
+	expected_1_href := "https://www.twitter.com/joncalhoun"
+	expected_2_text := "Gophercises is on Github!"
+	expected_2_href := "https://github.com/gophercises"
+	expected_printed_type := "[]glink.Link"
+	expected_length := 2
+
+	got_type := fmt.Sprintf("%T", result)
+    got_length := len(result)
+	if got_type != expected_printed_type {
+		t.Errorf("Expected type [%s], but got [%s]", expected_printed_type, got_type)
+	}
+    if got_length != expected_length {
+		t.Errorf("Expected length [%d], but got [%d]", expected_length, got_length)
+	}
+	if result[0].Text != expected_1_text {
+		t.Errorf("Expected [%s], but got [%s]", expected_1_text, result[0].Text)
+	}
+    if result[0].Href != expected_1_href {
+		t.Errorf("Expected href [%s], but got [%s]", expected_1_href, result[0].Href)
+	}
+	if result[1].Text != expected_2_text {
+		t.Errorf("Expected [%s], but got [%s]", expected_2_text, result[0].Text)
+	}
+    if result[1].Href != expected_2_href {
+		t.Errorf("Expected href [%s], but got [%s]", expected_2_href, result[0].Href)
+	}
+}
+
+
+func Test_dfsLink_from_node_with_many_subnodes_different_depth(t *testing.T) {
+	var l []Link
+	node_string := `<html>
+	<head>
+	</head>
+	<body>
+    	<div>
+            <div>
+	            <a href="https://www.twitter.com/joncalhoun">
+		            Check me out on twitter
+            	</a>
+            </div>
+    	</div>
+	    <a href="https://github.com/gophercises">
+		    Gophercises is on Github!
+    	</a>
+	</body>
+	</html>`
+	node := createNode(node_string)
+	result := dfsLink(node, &l)
+	expected_1_text := "Check me out on twitter"
+	expected_1_href := "https://www.twitter.com/joncalhoun"
+	expected_2_text := "Gophercises is on Github!"
+	expected_2_href := "https://github.com/gophercises"
+	expected_printed_type := "[]glink.Link"
+	expected_length := 2
+
+	got_type := fmt.Sprintf("%T", result)
+    got_length := len(result)
+	if got_type != expected_printed_type {
+		t.Errorf("Expected type [%s], but got [%s]", expected_printed_type, got_type)
+	}
+    if got_length != expected_length {
+		t.Errorf("Expected length [%d], but got [%d]", expected_length, got_length)
+	}
+	if result[0].Text != expected_1_text {
+		t.Errorf("Expected [%s], but got [%s]", expected_1_text, result[0].Text)
+	}
+    if result[0].Href != expected_1_href {
+		t.Errorf("Expected href [%s], but got [%s]", expected_1_href, result[0].Href)
+	}
+	if result[1].Text != expected_2_text {
+		t.Errorf("Expected [%s], but got [%s]", expected_2_text, result[0].Text)
+	}
+    if result[1].Href != expected_2_href {
+		t.Errorf("Expected href [%s], but got [%s]", expected_2_href, result[0].Href)
+	}
+}
